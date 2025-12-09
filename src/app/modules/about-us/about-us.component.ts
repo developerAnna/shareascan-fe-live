84a24d41314438001f40dc4bfd0b35a577507a4f
developerAnna <anna.developer07@gmail.com>
2025-12-09 19:25:41 +0530

    image and social lins changes

--- src/app/modules/about-us/about-us.component.ts
+++ src/app/modules/about-us/about-us.component.ts
@@ -3,6 +3,7 @@ import {TranslatePipe} from '@ngx-translate/core';
 import {BreadcrumbComponent} from '../../shared/components/breadcrumb/breadcrumb.component';
 import {FaqComponent} from '../../shared/components/faq/faq.component';
 import {SubscribeEmailComponent} from '../../shared/components/subscribe-email/subscribe-email.component';
+import {  ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
@@ -17,4 +18,21 @@ import {SubscribeEmailComponent} from '../../shared/components/subscribe-email/s
 })
 export class AboutUsComponent {
   public headerColor = signal<string>('#FF8A90');
+
+    @ViewChild('factoryVideo') factoryVideo!: ElementRef<HTMLVideoElement>;
+
+  playVideo() {
+    const video = this.factoryVideo.nativeElement;
+    const overlay = document.querySelector('.video-overlay') as HTMLElement;
+    const textOverlay = document.querySelector('.overlay-text') as HTMLElement;
+
+    overlay.style.display = 'none';
+    textOverlay.style.display = 'none';
+
+    video.play();
+  }
 }
+
+
+
+
